ad2221fd71fb5ac15df2290ee4a4e22eacd34036
AndyKan1984 <andishe.kanani@gmail.com>
2022-12-18 18:03:23 +0100

    Added models for expense and token, update the db

--- webapp/models.py
+++ webapp/models.py
@@ -8,3 +8,17 @@ class Income(models.Model):
     time = models.DateTimeField()
     note = models.CharField(max_length=128)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Expense(models.Model):
+    amount = models.BigIntegerField()
+    time = models.DateTimeField()
+    note = models.CharField(max_length=128)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Token(models.Model):
+    amount = models.BigIntegerField()
+    time = models.DateTimeField()
+    note = models.CharField(max_length=128)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
