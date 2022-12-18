a35deb5d3a69da633798b95e1118b2d71e0d28a1
AndyKan1984 <andishe.kanani@gmail.com>
2022-12-18 14:50:08 +0100

    back up setting + Fix models

---
+++ webapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Income(models.Model):
+    amount: models.BigIntegerField()
+    time: models.DateTimeField()
+    note: models.CharField(128)
+    user: models.ForeignKey(User, on_delete=models.CASCADE)
